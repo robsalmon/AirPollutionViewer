e48191a280e65afd59fadd58373ef125b0d95015
Robert Salmon <robdavidsalmon@msn.com>
2020-06-14 19:27:19 +0100

    Add basic view of pollution monitoring sites in Wandsworth

---
+++ scripts/app.js
@@ -0,0 +1,30 @@
+(function() { 
+var app = angular.module('airPollutionViewer', []);
+
+var MainController = function($scope, $http) {
+
+    var onLoad = function(response) {
+
+        $scope.pollutionData = response.data;
+        $scope.sites = response.data.DailyAirQualityIndex.LocalAuthority.Site;
+    
+    };
+
+    var onError = function(reason) {
+        $scope.error = reason;
+
+    };
+
+    $scope.getData = function() {
+
+$http.get("http://api.erg.kcl.ac.uk/AirQuality/Daily/MonitoringIndex/Latest/LocalAuthorityId=32/Json")
+.then(onLoad, onError);
+    };
+
+   
+
+};
+
+
+app.controller("MainController", ["$scope", "$http", MainController]);
+}());
